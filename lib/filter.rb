6f9ffc125f02ccd4f96d5db8bdb00e2e618a5f32
Sarthak Munshi <sarthakmunshi@gmail.com>
2016-11-13 23:06:33 +0530

    Specs: frequency_domain, spacial_domain

--- lib/filter.rb
+++ lib/filter.rb
@@ -6,25 +6,26 @@ class ButterworthFilter
 		@image_matrix = image_matrix
 		@rows = image_matrix.size
 		@columns = image_matrix[0].size
-		@row_split = @rows / 2 
-		@col_split = @col / 2 
+		@row_split = image_matrix.size / 2 
+		@col_split = image_matrix[0].size / 2 
 	end
 
 	def to_frequency_domain
-		auxiliary_matrix = NArray.to_na(@image_matrix.fft)
-		converted_matrix = FFTW3.fft(auxiliary_matrix).to_a
+		auxiliary_matrix = NArray.to_na(@image_matrix)
+		fft_matrix = FFTW3.fft(auxiliary_matrix) / auxiliary_matrix.length
+		converted_matrix = fft_matrix.to_a
 		return converted_matrix
 	end
 
 	def to_spacial_domain smoothened_matrix
 		auxiliary_matrix = NArray.to_na(smoothened_matrix)
-		converted_matrix = FFTW3.fft_bk(auxiliary_matrix).to_a
-		return converted_matrix
+		converted_matrix = FFTW3.ifft(auxiliary_matrix).real.to_a
+		integer_pixels = converted_matrix.map { |arr| arr.map { |ele| ele.to_i } }
+		return integer_pixels
 	end
 
 	def low_pass_filter n, cutoff_frequency, epsilon=1
 		processed_matrix = self.to_frequency_domain
-
 		processed_matrix.each_with_index do |current_row, xi|
 			current_row.each_with_index do |element, yi|
 				omega = Math.sqrt((xi - @row_split)**2 + (yi - @col_split)**2)
---
+++ spec/filter_spec.rb
@@ -0,0 +1,25 @@
+require 'filter.rb'
+
+describe ButterworthFilter do
+
+	test_image_array = [[1, 2], [3, 4]]
+	test_object = ButterworthFilter.new test_image_array
+	fft_output = [[(2.5+0.0i), (-0.5+0.0i)], [(-1.0+0.0i), (0.0+0.0i)]]
+
+	describe ".to_frequency_domain" do
+		context "given image array" do
+			it "returns complex FFTized array" do
+				expect(test_object.to_frequency_domain).to eql(fft_output)
+			end
+		end
+	end
+
+	describe ".to_spacial_domain" do
+		context "given FFT array" do
+			it "returns normal array" do
+				expect(test_object.to_spacial_domain(fft_output)).to eql(test_image_array)
+			end
+		end
+	end
+
+end
